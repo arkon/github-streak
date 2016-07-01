c481cf9d1795318ea89d0ec6c9b1612e0a0cd813
Eugene Cheung <ey.cheung@outlook.com>
2016-07-01 18:27:52 -0400

    Get dates

--- index.js
+++ index.js
@@ -1,69 +1,115 @@
-var contribGraph = document.querySelector('.js-calendar-graph-svg > g');
+const contribGraph = document.querySelector('.js-calendar-graph-svg > g');
 if (contribGraph) {
-    var contribs = contribGraph.querySelectorAll('rect.day');
-    var contribsLen = contribs.length - 1;
+  // HELPER FUNCTIONS
+  // ==========================================================================
 
-    var totalContribsNum = 0;
+  const getDate = function (contribNode) {
+    return contribNode.getAttribute('data-date');
+  };
 
-    var longestStreakNum = 0;
+  const resetStreak = function () {
+    return {
+      amount : 0,
+      start  : null,
+      end    : null
+    };
+  };
 
-    var isCurrentStreak = true;
-    var currentStreakNum = 0;
+  const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-    // Count some stuff
-    var streak = 0;
-    for (var i = contribsLen; i >= 0; i--) {
-        var count = parseInt(contribs[i].getAttribute('data-count'), 10);
+  const createDateString = function (date) {
+    const parsedDate = new Date(date);
 
-        // Tally up all contributions
-        totalContribsNum += count;
+    return `${MONTHS[parsedDate.getUTCMonth()].slice(0, 3)} ${parsedDate.getUTCDate()}, ${parsedDate.getUTCFullYear()}`;
+  };
 
-        if (count > 0) {
-            // If there's a contribution, add it to the current streak
-            streak++;
-        } else {
-            // End of streak (no contributions for this day)
+  const createStatDiv = function (header, type, data) {
+    const el = document.createElement('div');
 
-            // If not the current day and is the first streak, set the current streak
-            if (isCurrentStreak && i !== contribsLen) {
-                currentStreakNum = streak;
-                isCurrentStreak = false;
-            }
+    const elHeader = document.createElement('p');
+    elHeader.className = 'text-muted';
+    elHeader.textContent = header;
 
-            // Record if this is the longest streak encountered so far
-            if (streak > longestStreakNum) {
-                longestStreakNum = streak;
-            }
+    const elStat = document.createElement('h1');
+    elStat.textContent = `${data.amount} ${type}`;
 
-            // Reset streak
-            streak = 0;
-        }
+    const elFooter = document.createElement('p');
+    elHeader.className = 'text-muted';
+    elFooter.textContent = `${createDateString(data.start)} – ${createDateString(data.end)}`;
+
+    el.appendChild(elHeader);
+    el.appendChild(elStat);
+    el.appendChild(elFooter);
+
+    return el;
+  };
+
+  // DATA HOLDERS
+  // ==========================================================================
+
+  const contribContainer = document.getElementById('contributions-calendar');
+
+  const contribs    = contribGraph.querySelectorAll('rect.day');
+  const contribsLen = contribs.length - 1;
+
+  // Total contributions
+  let totalContribs   = resetStreak();
+  totalContribs.start = getDate(contribs[0]);
+  totalContribs.end   = getDate(contribs[contribsLen]);
+
+  // Longest streak
+  let longestStreak = resetStreak();
+
+  // Current streak
+  let isCurrentStreak = true;
+  let currentStreak   = resetStreak();
+
+
+  // TALLY UP THE STATS
+  // ==========================================================================
+
+  let streak = resetStreak();
+  let newStreak = true;
+
+  // Start counting from the end up
+  for (let i = contribsLen; i >= 0; i--) {
+    let count = parseInt(contribs[i].getAttribute('data-count'), 10);
+
+    // Tally up all contributions
+    totalContribs.amount += count;
+
+    if (count > 0) {
+      // If there's a contribution, add it to the current streak
+      streak.amount++;
+
+      // Record the end date for a new streak
+      if (newStreak) {
+        streak.end = getDate(contribs[i]);
+        newStreak = false;
+      }
+    } else {
+      // End of streak (no contributions for this day)
+      streak.start = getDate(contribs[i + 1]);
+
+      // If not the current day and is the first streak, set the current streak
+      if (isCurrentStreak && i !== contribsLen) {
+        currentStreak = streak;
+        isCurrentStreak = false;
+      }
+
+      // Record if this is the longest streak encountered so far
+      if (streak.amount > longestStreak.amount) {
+        longestStreak = streak;
+      }
+
+      // Reset streak
+      streak = resetStreak();
+      newStreak = true;
     }
+  }
 
-    // Append information to page
-    var contribContainer = document.getElementById('contributions-calendar');
-
-    var contribTotal = document.createElement('div');
-    contribTotal.innerHTML = 'Year of contributions: ' + totalContribsNum + ' total';
-    contribContainer.appendChild(contribTotal);
-
-    var contribLongestStreak = document.createElement('div');
-    contribLongestStreak.innerHTML = 'Longest streak: ' + longestStreakNum + ' days';
-    contribContainer.appendChild(contribLongestStreak);
-
-    var contribCurrentStreak = document.createElement('div');
-    contribCurrentStreak.innerHTML = 'Current streak: ' + currentStreakNum + ' days';
-    contribContainer.appendChild(contribCurrentStreak);
-
-    // Year of contributions
-    // 1,073 total
-    // Dec 21, 2013 - Dec 21, 2014
-
-    // Longest streak
-    // 365 days
-    // December 22 - December 21
-
-    // Current streak
-    // 365 days
-    // December 22 - Decemeber 21
+  // Append information to page
+  contribContainer.appendChild(createStatDiv('Year of contributions', 'total', totalContribs));
+  contribContainer.appendChild(createStatDiv('Longest streak', 'days', longestStreak));
+  contribContainer.appendChild(createStatDiv('Current streak', 'days', currentStreak));
 }
