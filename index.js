c96acfbe16af860d8eaecdeb03566c4a8f716832
Eugene Cheung <ey.cheung@outlook.com>
2016-07-01 17:31:40 -0400

    Get total contributions and longest streak

--- index.js
+++ index.js
@@ -1,22 +1,59 @@
 var contribGraph = document.querySelector('.js-calendar-graph-svg > g');
 if (contribGraph) {
     var contribs = contribGraph.querySelectorAll('rect.day');
+    var contribsLen = contribs.length - 1;
 
+    var totalContribsNum = 0;
+
+    var longestStreakNum = 0;
+
+    var isCurrentStreak = true;
+    var currentStreakNum = 0;
+
+    // Count some stuff
     var streak = 0;
+    for (var i = contribsLen; i >= 0; i--) {
+        var count = parseInt(contribs[i].getAttribute('data-count'), 10);
 
-    for (let i = contribs.length - 1; i >= 0; i--) {
-        if (contribs[i].getAttribute('data-count') !== '0') {
+        // Tally up all contributions
+        totalContribsNum += count;
+
+        if (count > 0) {
+            // If there's a contribution, add it to the current streak
             streak++;
         } else {
-            break;
+            // End of streak (no contributions for this day)
+
+            // If not the current day and is the first streak, set the current streak
+            if (isCurrentStreak && i !== contribsLen) {
+                currentStreakNum = streak;
+                isCurrentStreak = false;
+            }
+
+            // Record if this is the longest streak encountered so far
+            if (streak > longestStreakNum) {
+                longestStreakNum = streak;
+            }
+
+            // Reset streak
+            streak = 0;
         }
     }
 
+    // Append information to page
     var contribContainer = document.getElementById('contributions-calendar');
 
-    var contribStreakCount = document.createElement('div');
-    contribStreakCount.innerHTML = streak;
-    contribContainer.appendChild(contribStreakCount)
+    var contribTotal = document.createElement('div');
+    contribTotal.innerHTML = 'Year of contributions: ' + totalContribsNum + ' total';
+    contribContainer.appendChild(contribTotal);
+
+    var contribLongestStreak = document.createElement('div');
+    contribLongestStreak.innerHTML = 'Longest streak: ' + longestStreakNum + ' days';
+    contribContainer.appendChild(contribLongestStreak);
+
+    var contribCurrentStreak = document.createElement('div');
+    contribCurrentStreak.innerHTML = 'Current streak: ' + currentStreakNum + ' days';
+    contribContainer.appendChild(contribCurrentStreak);
 
     // Year of contributions
     // 1,073 total
